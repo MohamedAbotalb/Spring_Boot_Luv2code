d5a68e395e13daa60e36527819e5afb6129bbc47
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-08-31 20:52:16 +0300

    Add spring core - bean-lifecycle-methods

---
+++ Spring Core/8. bean-lifecycle-methods/src/main/java/com/mabotalb/spring_core_demo/common/BaseballCoach.java
@@ -0,0 +1,31 @@
+package com.mabotalb.spring_core_demo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+    public BaseballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // Define our init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // Define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes in batting practice";
+    }
+
+}
---
+++ Spring Core/8. bean-lifecycle-methods/src/main/java/com/mabotalb/spring_core_demo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.mabotalb.spring_core_demo.rest;
+
+import com.mabotalb.spring_core_demo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private final Coach myCoach;
+
+    // Define a constructor injection
+    @Autowired
+    public DemoController(@Qualifier("baseballCoach") Coach myCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return this.myCoach.getDailyWorkout();
+    }
+
+}
