b79d583d2a40689b807cca54e11174e43836a564
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-08-31 18:56:10 +0300

    Add spring core - qualifiers

---
+++ Spring Core/4. qualifiers/src/main/java/com/mabotalb/spring_core_demo/common/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.mabotalb.spring_core_demo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+
+}
---
+++ Spring Core/4. qualifiers/src/main/java/com/mabotalb/spring_core_demo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.mabotalb.spring_core_demo.rest;
+
+import com.mabotalb.spring_core_demo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private final Coach myCoach;
+
+    // Define a constructor injection
+    @Autowired
+    public DemoController(@Qualifier("trackCoach") Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return this.myCoach.getDailyWorkout();
+    }
+}
