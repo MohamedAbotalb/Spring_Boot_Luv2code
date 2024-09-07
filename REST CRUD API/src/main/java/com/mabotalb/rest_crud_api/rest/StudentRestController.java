a138bdd12c0c80c86275f451e75f74f0ede5017b
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-07 19:16:05 +0300

    Refactor exception handler to use global exception handling

--- REST CRUD API/src/main/java/com/mabotalb/rest_crud_api/rest/StudentRestController.java
+++ REST CRUD API/src/main/java/com/mabotalb/rest_crud_api/rest/StudentRestController.java
@@ -42,27 +42,4 @@ public class StudentRestController {
         }
         return this.students.get(id);
     }
-
-    // Add exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(ex.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception ex) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(ex.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
