a00edec50e6090160632b5b6e2bef1ff2b020595
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-09 01:05:36 +0300

    Add process form version three functionality

--- Spring MVC/src/main/java/com/mabotalb/thymeleaf_demo/controller/HelloWorldController.java
+++ Spring MVC/src/main/java/com/mabotalb/thymeleaf_demo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -31,6 +32,21 @@ public class HelloWorldController {
 
         // Create the message
         String result = "Yo! " + name +  "!!";
+
+        // Add the message to the model
+        model.addAttribute("message", result);
+
+        return "hello-world";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {
+
+        // Convert the data to uppercase
+        name = name.toUpperCase();
+
+        // Create the message
+        String result = "Hey my friend from v3 -> " + name +  "!!";
 
         // Add the message to the model
         model.addAttribute("message", result);
