f2837f4219140889783f1426f2a379c53b15c30e
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-09 17:55:54 +0300

    Add number range min and max validations

--- Spring MVC/Validations Demo/src/main/java/com/mabotalb/validations_demo/model/Customer.java
+++ Spring MVC/Validations Demo/src/main/java/com/mabotalb/validations_demo/model/Customer.java
@@ -1,5 +1,7 @@
 package com.mabotalb.validations_demo.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -11,11 +13,16 @@ public class Customer {
     @Size(min = 2, message = "Last name must be at least 2 characters long")
     private String lastName = "";
 
+    @Min(value = 0, message = "Free passes must be greater than or equal to 0")
+    @Max(value = 10, message = "Free passes must be less than or equal to 10")
+    private int freePasses;
+
     public Customer() {}
 
-    public Customer(String firstName, String lastName) {
+    public Customer(String firstName, String lastName, int freePasses) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.freePasses = freePasses;
     }
 
     public String getFirstName() {
@@ -32,5 +39,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
