fe58e5b8578122f9705e363b6e48d9924156b944
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-10 10:45:11 +0300

    Add update employee details functionality

--- Spring MVC CRUD/src/main/java/com/mabotalb/mvc_crud/controller/EmployeeController.java
+++ Spring MVC CRUD/src/main/java/com/mabotalb/mvc_crud/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.mabotalb.mvc_crud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -51,5 +48,17 @@ public class EmployeeController {
         employeeService.save(employee);
 
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+
+        // Retrieve the employee with the given ID from the database
+        Employee employee = employeeService.findById(id);
+
+        // Add the employee to the model
+        model.addAttribute("employee", employee);
+
+        return "employees/employee-form";
     }
 }
