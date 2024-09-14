ff23e0cf7ff074fbdc7eb329d55b511832266e62
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-14 16:19:09 +0300

    Add Spring MVC Security Default Version

---
+++ Spring MVC Security/Default/src/main/java/com/mabotalb/mvc_security_demo/MvcSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mabotalb.mvc_security_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ Spring MVC Security/Default/src/main/java/com/mabotalb/mvc_security_demo/security/DemoSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.mabotalb.mvc_security_demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+        @Bean
+        public InMemoryUserDetailsManager userDetailsManager() {
+
+                String password = "{noop}test123";
+
+                UserDetails john = User.builder()
+                                .username("john")
+                                .password(password)
+                                .roles("EMPLOYEE")
+                                .build();
+
+                UserDetails mary = User.builder()
+                                .username("mary")
+                                .password(password)
+                                .roles("EMPLOYEE", "MANAGER")
+                                .build();
+
+                UserDetails susan = User.builder()
+                                .username("susan")
+                                .password(password)
+                                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                                .build();
+
+                return new InMemoryUserDetailsManager(john, mary, susan);
+        }
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+                http.authorizeHttpRequests(configurer -> configurer
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+
+                                .anyRequest().authenticated())
+                                .formLogin(form -> form
+                                                .loginPage("/showLoginPage")
+                                                .loginProcessingUrl("/authenticateUser")
+                                                .permitAll())
+                                .logout(LogoutConfigurer::permitAll)
+                                .exceptionHandling(configurer -> configurer
+                                                .accessDeniedPage("/access-denied"));
+
+                return http.build();
+        }
+}
