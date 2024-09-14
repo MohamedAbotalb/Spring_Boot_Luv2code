5810a4381d087ee69bd5e6fc68c53f3a5f509338
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-14 11:15:21 +0300

    Add restrict urls based on roles functionality

--- Spring MVC Security/src/main/java/com/mabotalb/mvc_security_demo/security/DemoSecurityConfig.java
+++ Spring MVC Security/src/main/java/com/mabotalb/mvc_security_demo/security/DemoSecurityConfig.java
@@ -11,42 +11,47 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+        @Bean
+        public InMemoryUserDetailsManager userDetailsManager() {
 
-        String password = "{noop}test123";
+                String password = "{noop}test123";
 
-        UserDetails john = User.builder()
-                .username("john")
-                .password(password)
-                .roles("EMPLOYEE")
-                .build();
+                UserDetails john = User.builder()
+                                .username("john")
+                                .password(password)
+                                .roles("EMPLOYEE")
+                                .build();
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password(password)
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+                UserDetails mary = User.builder()
+                                .username("mary")
+                                .password(password)
+                                .roles("EMPLOYEE", "MANAGER")
+                                .build();
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password(password)
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+                UserDetails susan = User.builder()
+                                .username("susan")
+                                .password(password)
+                                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                                .build();
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
-    }
+                return new InMemoryUserDetailsManager(john, mary, susan);
+        }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/showLoginPage")
-                        .loginProcessingUrl("/authenticateUser")
-                        .permitAll())
-                .logout(logout -> logout.permitAll());
+                http.authorizeHttpRequests(configurer -> configurer
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
 
-        return http.build();
-    }
+                                .anyRequest().authenticated())
+                                .formLogin(form -> form
+                                                .loginPage("/showLoginPage")
+                                                .loginProcessingUrl("/authenticateUser")
+                                                .permitAll())
+                                .logout(logout -> logout.permitAll());
+
+                return http.build();
+        }
 }
