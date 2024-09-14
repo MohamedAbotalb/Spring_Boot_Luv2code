2453644a73aa08b3f58e8e7f62c17bc8f6fc55c6
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-14 18:26:14 +0300

    Add spring security with jdbc authorities

---
+++ Spring MVC Security/JDBC/src/main/java/com/mabotalb/mvc_security_demo/security/DemoSecurityConfig.java
@@ -0,0 +1,70 @@
+package com.mabotalb.mvc_security_demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+
+        // Add support for JDBC
+        @Bean
+        public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+                return new JdbcUserDetailsManager(dataSource);
+        }
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+                http.authorizeHttpRequests(configurer -> configurer
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+
+                                .anyRequest().authenticated())
+                                .formLogin(form -> form
+                                                .loginPage("/showLoginPage")
+                                                .loginProcessingUrl("/authenticateUser")
+                                                .permitAll())
+                                .logout(LogoutConfigurer::permitAll)
+                                .exceptionHandling(configurer -> configurer
+                                                .accessDeniedPage("/access-denied"));
+
+                return http.build();
+        }
+
+        /*
+        @Bean
+        public InMemoryUserDetailsManager userDetailsManager() {
+
+                String password = "{noop}test123";
+
+                UserDetails john = User.builder()
+                                .username("john")
+                                .password(password)
+                                .roles("EMPLOYEE")
+                                .build();
+
+                UserDetails mary = User.builder()
+                                .username("mary")
+                                .password(password)
+                                .roles("EMPLOYEE", "MANAGER")
+                                .build();
+
+                UserDetails susan = User.builder()
+                                .username("susan")
+                                .password(password)
+                                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                                .build();
+
+                return new InMemoryUserDetailsManager(john, mary, susan);
+        }
+        */
+}
---
+++ Spring MVC Security/JDBC/src/test/java/com/mabotalb/mvc_security_demo/MvcSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabotalb.mvc_security_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
